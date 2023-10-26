f53704641ffaab812c737f2e26a4b696fc255426
Sahan137 <144616822+Sahan137@users.noreply.github.com>
2023-10-26 14:56:28 +0200

    omgekeerd woord

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-
-def middel_getal():
+def omgekeerde_woord():
     while True:
-        getal1 = int(input("Vul hier het 1ste getal in: "))
-        getal2 = int(input("Vul hier het 2de getal in: "))
-        getal3 = int(input("Vul hier het 3de getal in: "))
+        woord=input("vul hier het woord in wat je wilt omdraaien: ")
+        omgekeerd_woord=""
+        for i in reversed(woord):
+            omgekeerd_woord += i
+        print(omgekeerd_woord)
 
-        if getal1 <= getal2 <= getal3 or getal3 <= getal2 <= getal1:
-            middelste_getal=getal2
-            print(middelste_getal)
-
-        elif getal2 <= getal1 <= getal3 or getal3 <= getal1 <= getal2:
-            middelste_getal=getal1
-            print(middelste_getal)
-
-        else:
-            middelste_getal=getal3
-            print(middelste_getal)
-
-
-middel_getal()
+omgekeerde_woord()
